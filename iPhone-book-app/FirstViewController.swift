854cd8170294eca6f2ae537d4309a7ee9cc07650
Tomislav Gajsak <tomislav.gajsak@comsysto.com>
2015-01-12 11:39:05 +0100

    Initial commit

---
+++ iPhone-book-app/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iPhone-book-app
+//
+//  Created by Tomislav Gajsak on 12/01/15.
+//  Copyright (c) 2015 Tomislav Gajsak. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iPhone-book-app/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iPhone-book-app
+//
+//  Created by Tomislav Gajsak on 12/01/15.
+//  Copyright (c) 2015 Tomislav Gajsak. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
